fcafce52365c9a3ebbf586f52e270cbb9241887f
alifanrenaldi <83490686+alifanrenaldi@users.noreply.github.com>
2021-07-08 23:26:24 +0700

    Update Parsing_String_Infix.cpp

--- Parsing_String_Infix.cpp
+++ Parsing_String_Infix.cpp
@@ -23,35 +23,37 @@ void SosialDistancingInfix(string Operator){
             }
         }
   
+        
+        
+        
         else if(Operator[p]=='-'){
-            
             if (isdigit (Operator[p-1]) && Operator[p+1] == '(') {
                 cout << Operator[p] << " ";
             }
             else if (!isdigit(Operator[p-1]) && Operator[p+1] == '('){
-                  cout << "-1 * ";
+                cout << "-1 * ";
             }
             else if (!isdigit(Operator[p-1]) && Operator[p+1] == ')'){
-                  cout << Operator[p] << " ";
+                cout << Operator[p] << " ";
             }
             else if (Operator[p-1] == ' ' && Operator[p+1] == ' '){
-                  cout << Operator[p] << " ";
+                cout << Operator[p] << " ";
             }
             else if (isdigit(Operator[p-1])){
-                  cout << Operator[p] << " ";
+                cout << Operator[p] << " ";
             }
-           }
+        }
         else if (Operator [p] == ' '){
-           }
+        }
         else if (isdigit(Operator[p])){
             if(isdigit(Operator[p+1])){
-                  cout << Operator [p];
+                cout << Operator [p];
             }
             else if (!isdigit(Operator[p+1])){
-                  cout << Operator[p] << " ";
+                cout << Operator[p] << " ";
             }
         }
         else
-               cout << Operator[p] << " ";
+            cout << Operator[p] << " ";
     }
 };
