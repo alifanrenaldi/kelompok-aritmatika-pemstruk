f57e3ff7cf24dedd3026954e1526e1de1cb19712
SaddamSurya <86936661+SaddamSurya@users.noreply.github.com>
2021-07-08 22:58:21 +0700

    Update parsing.cpp

--- parsing.cpp
+++ parsing.cpp
@@ -1,3 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void SosialDistancingInfix(string Operator){
+    
+    for (int p=0; p<Operator.length(); p++) {   
+        if(p==0 && Operator[p] == '-' && Operator[p+1]!= '('){
+            cout << Operator[p];
+        }
+        if(Operator[p] == '('){
+            if(Operator[p] == '(' && isdigit(Operator[p-1]) && (Operator[p-1]) != '-' && (Operator[p-1]) != ' '){
+                cout << "* ";
+            }    
+        }
+        if((Operator[p]=='-') && (Operator[p-1]=='+' || Operator[p-1]=='*' || Operator[p-1]=='/' || Operator[p-1]=='%')){
+            if(!isdigit(Operator[p]=='-') && (Operator[p-1]=='+' || Operator[p-1]=='*' || Operator[p-1]=='/' ||Operator[p-1]=='%')){
+                cout << "-1 * ";
+            }
+            else if(isdigit(Operator[p]=='-') && (Operator[p-1]=='+' || Operator[p-1]=='*' || Operator[p-1]=='/' || Operator[p-1]=='%')){
+                cout << Operator[p] << " ";
+            }
+            else if(isdigit(Operator[p-1])){
+                cout << Operator[p] << " ";
+            }
+        }
+                
 else if(Operator[p]=='-'){
             if (isdigit (Operator[p-1]) && Operator[p+1] == '(') {
                 cout << Operator[p] << " ";
