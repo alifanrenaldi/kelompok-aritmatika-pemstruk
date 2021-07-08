a42eda0dbd5e3fd363b2afc3c84bf40a49cc009f
alifanrenaldi <83490686+alifanrenaldi@users.noreply.github.com>
2021-07-08 23:15:08 +0700

    Update Parsing_String_Infix.cpp
    tambah codingan guys

--- Parsing_String_Infix.cpp
+++ Parsing_String_Infix.cpp
@@ -22,3 +22,35 @@ void SosialDistancingInfix(string Operator){
                 cout << Operator[p] << " ";
             }
         }
+        
+ else if(Operator[p]=='-'){
+            if (isdigit (Operator[p-1]) && Operator[p+1] == '(') {
+                cout << Operator[p] << " ";
+            }
+            else if (!isdigit(Operator[p-1]) && Operator[p+1] == '('){
+                cout << "-1 * ";
+            }
+            else if (!isdigit(Operator[p-1]) && Operator[p+1] == ')'){
+                cout << Operator[p] << " ";
+            }
+            else if (Operator[p-1] == ' ' && Operator[p+1] == ' '){
+                cout << Operator[p] << " ";
+            }
+            else if (isdigit(Operator[p-1])){
+                cout << Operator[p] << " ";
+            }
+        }
+        else if (Operator [p] == ' '){
+        }
+        else if (isdigit(Operator[p])){
+            if(isdigit(Operator[p+1])){
+                cout << Operator [p];
+            }
+            else if (!isdigit(Operator[p+1])){
+                cout << Operator[p] << " ";
+            }
+        }
+        else
+            cout << Operator[p] << " ";
+    }
+};
