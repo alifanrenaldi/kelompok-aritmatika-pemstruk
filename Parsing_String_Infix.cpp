acd0891cc4dc19b71a9de1cc217e88efc0e81144
KurniawanDwiYuliyanto2057051010 <83492070+KurniawanDwiYuliyanto2057051010@users.noreply.github.com>
2021-07-08 09:27:11 -0700

    Create Parsing_String_Infix.cpp
    nambahin main.  jangan di ubah lagi

--- Parsing_String_Infix.cpp
+++ Parsing_String_Infix.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-void SosialDistancingInfix(string Operator){
-    
+
+void PPKM_DARURAT(string Operator){    
     for (int p=0; p<Operator.length(); p++) {   
         if(p==0 && Operator[p] == '-' && Operator[p+1]!= '('){
             cout << Operator[p];
@@ -23,37 +23,41 @@ void SosialDistancingInfix(string Operator){
             }
         }
   
-        
-        
-        
         else if(Operator[p]=='-'){
+            
             if (isdigit (Operator[p-1]) && Operator[p+1] == '(') {
                 cout << Operator[p] << " ";
             }
             else if (!isdigit(Operator[p-1]) && Operator[p+1] == '('){
-                cout << "-1 * ";
+                  cout << "-1 * ";
             }
             else if (!isdigit(Operator[p-1]) && Operator[p+1] == ')'){
-                cout << Operator[p] << " ";
+                  cout << Operator[p] << " ";
             }
             else if (Operator[p-1] == ' ' && Operator[p+1] == ' '){
-                cout << Operator[p] << " ";
+                  cout << Operator[p] << " ";
             }
             else if (isdigit(Operator[p-1])){
-                cout << Operator[p] << " ";
+                  cout << Operator[p] << " ";
             }
-        }
+           }
         else if (Operator [p] == ' '){
-        }
+           }
         else if (isdigit(Operator[p])){
             if(isdigit(Operator[p+1])){
-                cout << Operator [p];
+                  cout << Operator [p];
             }
             else if (!isdigit(Operator[p+1])){
-                cout << Operator[p] << " ";
+                  cout << Operator[p] << " ";
             }
         }
         else
-            cout << Operator[p] << " ";
+               cout << Operator[p] << " ";
     }
 };
+
+int main (){
+    string Operator;
+    getline(cin, Operator);
+    PPKM_DARURAT(Operator);
+}
