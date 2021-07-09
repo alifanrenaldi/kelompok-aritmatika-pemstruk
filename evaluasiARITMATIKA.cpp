c63cdaecea954cd01f6d2361565a1ddc63ead564
SaddamSurya <86936661+SaddamSurya@users.noreply.github.com>
2021-07-09 21:43:08 +0700

    Update evaluasiARITMATIKA.cpp
    nambah codingan

--- evaluasiARITMATIKA.cpp
+++ evaluasiARITMATIKA.cpp
@@ -47,3 +47,100 @@ double run (double x, double y, string n){
     }
     return 0;
 }
+
+vector <string> infix;
+vector <string> postfix;
+string temp;
+vector <string>::iterator itr;
+vector <string>::iterator it;
+vector <string>::iterator iter;
+
+void input(){
+    string str;
+    char dam;
+    while(cin.get(dam)){
+        if(dam == '\n'){
+            break;
+        }
+        if(dam != ' '){
+            str.push_back(dam);
+        }
+    }
+    int length = str.length();
+
+    for(int i = 0; i < length; i++){
+        if(str[i] == '(' || str[i] == ')'){
+            if(temp.length() != 0){
+                infix.push_back(temp);
+                temp.clear();
+            }
+            infix.push_back(str.substr(i, 1));
+        }
+        if(isdigit(str[i]) || (str[i] == '-' && i == 0 && isdigit(str[i+1]))){
+            temp.push_back(str[i]);
+            continue;
+        }
+        if(isOperator(str[i])){
+            if(temp.length() != 0){
+                infix.push_back(temp);
+                temp.clear();
+            }
+            if((str[i] == '-' && isOperator(str[i-1])) || 
+               (str[i] == '-' && i == 0 && str[i+1] == '(') || 
+               (str[i] == '-' && str[i-1] == '(')){
+                infix.push_back("-1");
+                infix.push_back("*");
+            }else{
+                infix.push_back(str.substr(i, 1));
+            }
+        }
+    }
+    if(temp.length() != 0){
+        infix.push_back(temp);
+        temp.clear();
+    }
+}
+
+void toPostfix(){
+    stack <string> lipan;
+    int i = 0;
+    for(itr = infix.begin(); itr != infix.end() ; itr++, i++){
+        if(isdigit(infix[i].back())){
+            postfix.push_back(infix[i]);
+            continue;
+        }
+        if(infix[i] == "("){
+            lipan.push(infix[i]);
+            continue;
+        }
+        if(infix[i] == ")"){
+            while(!lipan.empty() && (lipan.top() != "(")){
+                string toPush = lipan.top();
+                postfix.push_back(toPush);
+                lipan.pop();
+            }
+            lipan.pop();
+            continue;
+        }
+        if(isOperator(infix[i][0])){
+            if(lipan.empty() || lipan.top() == "("){
+                lipan.push(infix[i]);
+            }
+            else{
+                while(!lipan.empty() && (lipan.top() != "(") && 
+                      large(infix[i], lipan.top())){
+                    string toPush = lipan.top();
+                    postfix.push_back(toPush);
+                    lipan.pop();
+                }
+                lipan.push(infix[i] );
+            }
+            continue;
+        }
+    }
+    while(!lipan.empty()){
+        string toPush = lipan.top();
+        postfix.push_back(toPush);
+        lipan.pop();
+    }
+}
