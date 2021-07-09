f4db8d6e9a2f0124dfe95982904ad5b421e712ef
alifanrenaldi <83490686+alifanrenaldi@users.noreply.github.com>
2021-07-09 22:05:35 +0700

    Update infixtopostfix.cpp
    membuat coding guys 1

--- infixtopostfix.cpp
+++ infixtopostfix.cpp
@@ -1,1 +1,40 @@
 # kelompok-aritmatika-pemstruk
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool oibreoir(char r){
+    if(r == '+' || 
+       r == '-' || 
+       r == '*' || 
+       r == '/' || 
+       r == '%'   ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int prioritet(string r){
+    if(r == "+" ||
+       r == "-"   ){
+        return 1;
+    }
+    else if(r == "*" ||
+            r == "/" ||
+            r == "%"   ){
+        return 2;
+    }
+    return 0;
+}
+
+bool grande(string r, string s){
+    int Nilai1, Nilai2;
+    Nilai1 = prioritet(r); Nilai2 = prioritet(s);
+    
+    return Nilai1 <= Nilai2;
+}
+
+vector <string> infix; vector <string> postfix; string temp; vector <string>::iterator remake; vector <string>::iterator itu;
